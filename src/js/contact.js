447176313e9d683bbaf02b94552f8a528e02a6fd
Damian <damianjagodaz@gmail.com>
2023-01-06 14:16:55 +0100

    odified files

---
+++ src/js/contact.js
@@ -0,0 +1,29 @@
+// const form = document.querySelector('#contact-container__form-box')
+
+// const submitForm = event => {
+// 	event.preventDefault()
+// 	let topic = event.target[0].value
+// 	let email = event.target[1].value
+// 	let message = event.target[2].value
+
+//     fetch('localhost:5001/api/contact', "post", {
+//         topic: topic,
+//         email: email,
+//         message: message
+//     })
+// }
+
+// form.addEventListener('submit', submitForm)
+// console.log('asasasd213')
+
+
+// @PostMapping("/contact")
+// Employee newEmployee(@RequestBody Contact contact) {
+//   emailService.sendMessage(contact.topic, contact.email, contact.message)
+// }
+
+// public class Contact {
+//     string topic;
+//     string email;
+//     string message;
+// }
